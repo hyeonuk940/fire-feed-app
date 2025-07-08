a55ba28a8457877f938222af2796bd796a659207
hyeonuk940 <2342849@donga.ac.kr>
2025-07-08 18:01:45 +0900

    feed firebase 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:fire_feed_app/screen/home_screen.dart';
 import 'package:fire_feed_app/screen/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
