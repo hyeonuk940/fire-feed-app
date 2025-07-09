daee1656f954eff66e02727b33cb18b33444604f
chan <chanju0716@naver.com>
2025-07-09 15:55:45 +0900

    a

--- lib/models/feed.dart
+++ lib/models/feed.dart
@@ -1,19 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Feed {
   final String title;
   final String content;
   final String imageUrl;
+  final Timestamp createdAt;
+  final String id;
 
   Feed({
+    required this.id,
     required this.title,
     required this.content,
     required this.imageUrl,
+    required this.createdAt,
   });
 
-  factory Feed.fromJson(Map<String, dynamic> json) {
-    return Feed(
-      title: json['title'],
-      content: json['content'],
-      imageUrl: json['imageUrl'],
-    );
+  factory Feed.fromFirestore(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Feed(id: doc.id, title: data['title'] ?? '', content: data['content'] ?? '', imageUrl: data['imageUrl'] ?? '',
+      createdAt: data['createdAt'] ?? Timestamp.now(),);
   }
 }
